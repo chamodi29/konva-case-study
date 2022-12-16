d8f0d388244ca7a46b65b88d42d6aaab6da88402
chamodi29 <rosanjala29@gmail.com>
2022-12-16 15:58:15 +0530

    Generating random, draggable circles using Konva

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,73 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createRoot } from 'react-dom/client';
+import { Stage, Layer, Circle, Text } from 'react-konva';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function generateShapes() {
+  return [...Array(8)].map((_, i) => ({
+    id: i.toString(),
+    x: Math.random() * window.innerWidth,
+    y: Math.random() * window.innerHeight,
+    rotation: Math.random() * 180,
+    isDragging: false,
+  }));
 }
+
+const INITIAL_STATE = generateShapes();
+
+const App = () => {
+  const [circles, setCircles] = React.useState(INITIAL_STATE);
+
+  const handleDragStart = (e:any) => {
+    const id = e.target.id();
+    setCircles(
+      circles.map((circle) => {
+        return {
+          ...circle,
+          isDragging: circle.id === id,
+        };
+      })
+    );
+  };
+  const handleDragEnd = (e: any) => {
+    setCircles(
+      circles.map((circle) => {
+        return {
+          ...circle,
+          isDragging: false,
+        };
+      })
+    );
+  };
+
+  return (
+    <Stage width={window.innerWidth} height={window.innerHeight}>
+      <Layer>
+        <Text text="You can drag any circle here" />
+        {circles.map((circle) => (
+          <Circle
+            key={circle.id}
+            id={circle.id}
+            x={circle.x}
+            y={circle.y}
+            radius={40}
+            fill="#FE74D0"
+            opacity={0.8}
+            draggable
+            rotation={circle.rotation}
+            shadowColor="black"
+            shadowBlur={10}
+            shadowOpacity={0.6}
+            shadowOffsetX={circle.isDragging ? 10 : 5}
+            shadowOffsetY={circle.isDragging ? 10 : 5}
+            scaleX={circle.isDragging ? 1.2 : 1}
+            scaleY={circle.isDragging ? 1.2 : 1}
+            onDragStart={handleDragStart}
+            onDragEnd={handleDragEnd}
+          />
+        ))}
+      </Layer>
+    </Stage>
+  );
+};
 
 export default App;
