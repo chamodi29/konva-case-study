c53b0fcbb130ad9683b4d565bfa27ee8d6469c2c
chamodi29 <rosanjala29@gmail.com>
2022-12-30 17:12:05 +0530

    Highlighter changes store in locally

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,107 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Stage, Layer, Circle, Text } from 'react-konva';
-
-function generateShapes() {
-  return [...Array(8)].map((_, i) => ({
-    id: i.toString(),
-    x: Math.random() * window.innerWidth,
-    y: Math.random() * window.innerHeight,
-    rotation: Math.random() * 180,
-    isDragging: false,
-  }));
-}
-
-const INITIAL_STATE = generateShapes();
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { Stage, Layer, Line, Text } from 'react-konva'
+import { useEffect } from 'react'
 
 const App = () => {
-  const [circles, setCircles] = React.useState(INITIAL_STATE);
+  const [tool, setTool] = React.useState('pen')
+  const [lines, setLines] = React.useState([] as any[])
+  const isDrawing = React.useRef(false)
 
-  const handleDragStart = (e:any) => {
-    const id = e.target.id();
-    setCircles(
-      circles.map((circle) => {
-        return {
-          ...circle,
-          isDragging: circle.id === id,
-        };
-      })
-    );
-  };
-  const handleDragEnd = (e: any) => {
-    setCircles(
-      circles.map((circle) => {
-        return {
-          ...circle,
-          isDragging: false,
-        };
-      })
-    );
-  };
+  useEffect(() => {
+    //get lines from localstorage
+    const linesString = window.localStorage.getItem('Lines') as string
+    console.log(linesString)
+    if (linesString) {
+      const linesArray = JSON.parse(linesString)
+      console.log(linesArray)
+      setLines(linesArray)
+    }
+    return () => {}
+  }, [])
+
+  //setLines
+  const handleMouseDown = (e: any) => {
+    isDrawing.current = true
+    const pos = e.target.getStage().getPointerPosition()
+    const updatedLines = [...lines, { tool, points: [pos.x, pos.y] }]
+    setLines(updatedLines)
+  }
+
+  const handleMouseMove = (e: any) => {
+    // no drawing - skipping
+    if (!isDrawing.current) {
+      return
+    }
+    const stage = e.target.getStage()
+    const point = stage.getPointerPosition()
+    let lastLine = lines[lines.length - 1]
+    // add point
+    lastLine.points = lastLine.points.concat([point.x, point.y])
+
+    // replace last
+    lines.splice(lines.length - 1, 1, lastLine)
+    setLines(lines.concat())
+  }
+
+  const handleMouseUp = () => {
+    isDrawing.current = false
+
+    //todo: stringify updated lines
+    var myJsonString = JSON.stringify(lines)
+    //save result in local storage
+    window.localStorage.setItem('Lines', myJsonString)
+  }
+
+  const handleClear = () => {
+    //todo: stringify updated lines
+    var myJsonString = JSON.stringify([])
+    setLines([])
+    //save result in local storage
+    window.localStorage.setItem('Lines', myJsonString)
+  }
 
   return (
-    <Stage width={window.innerWidth} height={window.innerHeight}>
-      <Layer>
-        <Text text="You can drag any circle here" />
-        {circles.map((circle) => (
-          <Circle
-            key={circle.id}
-            id={circle.id}
-            x={circle.x}
-            y={circle.y}
-            radius={40}
-            fill="#FE74D0"
-            opacity={0.8}
-            draggable
-            rotation={circle.rotation}
-            shadowColor="black"
-            shadowBlur={10}
-            shadowOpacity={0.6}
-            shadowOffsetX={circle.isDragging ? 10 : 5}
-            shadowOffsetY={circle.isDragging ? 10 : 5}
-            scaleX={circle.isDragging ? 1.2 : 1}
-            scaleY={circle.isDragging ? 1.2 : 1}
-            onDragStart={handleDragStart}
-            onDragEnd={handleDragEnd}
-          />
-        ))}
-      </Layer>
-    </Stage>
-  );
-};
+    <div>
+      <select
+        value={tool}
+        onChange={(e) => {
+          setTool(e.target.value)
+        }}
+      >
+        <option value="pen">Pen</option>
+        <option value="eraser">Eraser</option>
+      </select>
+      <button onClick={handleClear}>clear</button>
+      <Stage
+        width={window.innerWidth}
+        height={window.innerHeight}
+        onMouseDown={handleMouseDown}
+        onMousemove={handleMouseMove}
+        onMouseup={handleMouseUp}
+      >
+        <Layer>
+          <Text text="Just start drawing" x={5} y={30} />
+          <Text text="Erase" x={10} y={60} />
+          {lines.map((line, i) => (
+            <Line
+              key={i}
+              points={line.points}
+              stroke="#FFF753"
+              strokeWidth={10}
+              tension={0.5}
+              lineCap="round"
+              lineJoin="round"
+              opacity={0.5}
+              globalCompositeOperation={
+                line.tool === 'eraser' ? 'destination-out' : 'source-over'
+              }
+            />
+          ))}
+        </Layer>
+      </Stage>
+    </div>
+  )
+}
 
-export default App;
+export default App
